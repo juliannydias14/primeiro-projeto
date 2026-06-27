d83d945033e5d45832d4be9618869c1e4aad4044
juliannydias14 <juliannydias14@gmail.com>
2026-06-26 21:21:24 -0300

    vetores

---
+++ atividades/vetores.py
@@ -0,0 +1,14 @@
+fruta = ["Melancia", "Pera", "Uva" , "Laranja", "Acerola", "Jabuticaba"]
+#print(fruta[4])
+
+# print("\n frutas ". join(fruta))
+
+fruta.sort()
+
+for i in range(len(fruta)):
+    print(fruta[i])
+
+for i in fruta:
+    print(i)
+
+
